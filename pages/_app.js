f47bcd00957e15210b3016910e93e4e4b4edf43e
Jim Fang <jfang.cv.ca.us@gmail.com>
2022-02-27 06:30:47 -0800

    some progress...

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import Nav from '../globalCompoents/Nav'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className='h-screen bg-black text-white'>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
