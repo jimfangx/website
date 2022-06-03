bcdfff3bb9e5ee4af1b84e3e6a0ba853df442bf2
Jim Fang <jfang.cv.ca.us@gmail.com>
2022-06-02 23:18:00 -0700

    remove resume for now

--- components/Links.js
+++ components/Links.js
@@ -5,9 +5,9 @@ import LinkNext from 'next/link'
 export default function Links() {
     return (
         <div className='flex justify-center text-lg animate-fade'>
-            <a href="/resume-Jim-Fang.pdf" className='flex flex-row justify-between m-4 underline decoration-dotted'>
+            {/* <a href="/resume-Jim-Fang.pdf" className='flex flex-row justify-between m-4 underline decoration-dotted'>
                 Resume
-            </a>
+            </a> */}
             <a href="https://github.com/AirFusion45?tab=repositories" className="flex flex-row justify-between m-4 underline decoration-dotted" target='_blank' rel='noreferrer'>
                 Projects <FaExternalLinkAlt className="ml-2 mt-2 text-sm" />
             </a>
