7c38c8b05a18efb5f57a43ca1027b3df11a1275b
Jim Fang <jfang.cv.ca.us@gmail.com>
2023-07-08 20:56:40 +0800

    fixed tab names & favicons

--- pages/uses.js
+++ pages/uses.js
@@ -43,7 +43,7 @@ export default function Uses() {
     return (
         <div className='bg-bgBlack text-white flex items-center flex-col overflow-hidden'>
             <Head>
-                <title>Tech Stack & Gear | Jim Fang</title>
+                <title>Development Stack & Tooling | airfusion.dev</title>
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
